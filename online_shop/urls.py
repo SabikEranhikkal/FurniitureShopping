366f674ec787f3ac382c4125c323e9c468d6213a
SabikEranhikkal <sabikeranhikkal123@gmail.com>
2024-07-02 18:26:49 +0530

    Push Project

---
+++ online_shop/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('shop.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('cart/', include('cart.urls')),
+    path('orders/', include('orders.urls')),
+    path('dashboard/', include('dashboard.urls')),
+    
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
